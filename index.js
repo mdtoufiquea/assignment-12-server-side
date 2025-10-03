3b5ffdb04a1a706e9cc6016bab56f7d22ca28ac3
mdtoufique <toufiquealam0200@gmail.com>
2025-10-03 22:14:14 +0600

    added all code my project

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db('ScholarX');
     const usersCollection = db.collection('users');
@@ -499,8 +499,8 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close();
   }
